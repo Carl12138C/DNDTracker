f4441570c2109f1a96019e063ad132a6e9050149
Sunny Chan <schan6803@gmail.com>
2024-01-21 22:11:14 -0500

    navbar stuff

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,6 +3,7 @@ export default function Error() {
     return (
         <div>
             <h1>Ayo home boi you came to the wrong house fool</h1>
+            {/* <img src ="https://variety.com/wp-content/uploads/2021/07/Rick-Astley-Never-Gonna-Give-You-Up.png?w=1000&h=563&crop=1"></img> */}
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,33 @@
-import Button from '@mui/material/Button';
-import { ButtonGroup } from '@mui/material';
-import { Link } from 'react-router-dom';
+import Button from "@mui/material/Button";
+import { ButtonGroup } from "@mui/material";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { fontSize } from "@mui/system";
 
-export default function Navbar(){
-    return (
-        <div class="navbar-group">
-            <div class="link-border">
-                 <Link to= "/character">
-                    <span>Character</span>
-                 </Link>
-                 
-            </div>
-            <div class="link-border">
-                <Link to= "/roll">
-                    <span>Roll</span>
-                </Link>
-            </div>
-        </div>
-    )
+export default function Navbar() {
+  return (
+    <div className="navbar-group">
+      <div className="link-border">
+        <Link to="">
+            <span>Home</span>
+        </Link>
+      </div>        
+      <div className="link-border">
+        <Link to="/character">
+          <span>Character</span>
+        </Link>
+      </div>
+      <div className="link-border">
+        <Link to="/roll">
+          <span>Roll</span>
+        </Link>
+      </div>
+      <div className="link-border">
+        <Link to="/library">
+            <span>Library</span>
+        </Link>
+      </div>
+
+    </div>
+  );
 }
