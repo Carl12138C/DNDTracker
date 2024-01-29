2ceb37d018e305298e95f2d081a04df532cc7222
Carl <shiyangcai12138@gmail.com>
2024-01-29 00:39:26 -0500

    added layout for charater scores

--- src/components/Character/CharacterScores.jsx
+++ src/components/Character/CharacterScores.jsx
@@ -2,7 +2,27 @@ import "./character.css";
 export default function CharacterScores() {
   return (
     <div id="container">
-      <p>Character Scores</p>
+      <div id="label-div">
+        <label>Name: Temporary</label>
+      </div>
+      <div id="top">
+        <div id="ability-scores-container">
+            <label>Str: 10 +5</label>
+            <label>Dex: 10 +5</label>
+            <label>Con: 10 +5</label>
+            <label>Int: 10 +5</label>
+            <label>Wis: 10 +5</label>
+            <label>Cha: 10 +5</label>
+            <label>Proficiency bonus +5</label>
+
+        </div>
+        <div id="character-img-container">
+            <p>right</p>
+        </div>
+      </div>
+      <div id="bottom">
+        <p>bottom</p>
+      </div>
     </div>
   );
 }
